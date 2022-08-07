b0e31411c3f1aa372407eab7fec158797b6c585e
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-07 18:49:23 +0000

    Setup allauth with email veryfication

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = 'django-insecure-=-99ceb=^)x2^-%9^@nnslv_m%fc@19$gs$66apv+prtjij6oz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ['https://8000-anelairam-kaleidoscope-1ednhrzx19y.ws-eu59.gitpod.io']
+
 ALLOWED_HOSTS = []
 
 
@@ -37,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +72,26 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'kaleidoscope.wsgi.application'
 
