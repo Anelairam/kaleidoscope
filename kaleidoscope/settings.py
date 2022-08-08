d7d80d8e7097bd15da7508d2ccfbf53a0cc0a360
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-08 19:54:18 +0000

    Create the portfolio app

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
 
     'home',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -148,6 +149,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
