5f4e6ff9bfa017cb8b9529e2d0bce101a3d74841
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-08 20:05:01 +0000

    debugging media

--- portfolio/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Category(models.Model):
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
