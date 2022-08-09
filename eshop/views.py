976429461a3dab416ba5c999a394c566b022d2f9
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-09 19:19:25 +0000

    eshop app setup completed and rendered

--- eshop/views.py
+++ eshop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import OnlineCourse, Tour
 
 # Create your views here.
 
 def eshop(request):
     """The view returns the index page"""
-    return render(request, 'eshop/eshop.html')
+    online_courses = OnlineCourse.objects.all()
+    tours = Tour.objects.all()
+
+    context = {
+        'onlinecourses': online_courses,
+        'tours': tours,
+    }
+    return render(request, 'eshop/eshop.html', context)
