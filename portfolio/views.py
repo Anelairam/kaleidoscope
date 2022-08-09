a3c14aceb5569e42065a0921febbc38e1744f2d7
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-09 18:40:19 +0000

    Render photo in portfolio app

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
 
 def portfolio(request):
     """The view returns the index page"""
-    return render(request, 'portfolio/portfolio.html')
+    photos = Photo.objects.all()
+
+    context = {
+        'photos': photos,
+    }
+
+    return render(request, 'portfolio/portfolio.html', context)
