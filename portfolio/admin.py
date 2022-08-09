0d6fe7d30f6ef5af60ee8a362a3b1023b72d5f3b
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-09 18:33:02 +0000

    Customise the admin site

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Photo, Category
 # Register your models here.
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'label',
+        'price',
+        'sku',
+        'size',
+        'image',
+    )
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+admin.site.register(Photo, PhotoAdmin)
+admin.site.register(Category, CategoryAdmin)
