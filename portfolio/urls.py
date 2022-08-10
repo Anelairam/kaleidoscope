fa0cdd34903223c764c1eb2c6702f6b905dbc76e
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-10 19:01:33 +0000

    Add photo details functionallity

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.portfolio, name='portfolio'),
+    path('<photo_id>', views.photo_detail, name='photo_detail'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Photo
 
 # Create your views here.
@@ -12,3 +12,14 @@ def portfolio(request):
     }
 
     return render(request, 'portfolio/portfolio.html', context)
+
+
+def photo_detail(request, photo_id):
+    """The view returns the index page"""
+    photo = get_object_or_404(Photo, pk=photo_id)
+
+    context = {
+        'photos': photo,
+    }
+
+    return render(request, 'portfolio/photo_detail.html', context)
