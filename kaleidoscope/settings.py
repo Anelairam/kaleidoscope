436913b61684e1345b4b7585cc0283b99684d994
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-13 20:50:20 +0000

    Responsive

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -176,7 +176,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = photo@photo.com
+    # DEFAULT_FROM_EMAIL = photo@photo.com
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
