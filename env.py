48815f04d4fd5b3efab168ae0afac25aac9aa80e
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-08-13 13:15:44 +0000

    env.py

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://iognearkcygabp:e7fff6eb68f2afcc0400ab753f031f3b388d030bc5185ecb00b168977d759165@ec2-54-246-185-161.eu-west-1.compute.amazonaws.com:5432/df2ubanud6vvhr"
+os.environ["SECRET_KEY"] = "randomSecretKey4193!!!"
