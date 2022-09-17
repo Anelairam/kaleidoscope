0f01ff843dbd3c1773e524f003a38e5da801e834
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-17 18:56:01 +0000

    No change

--- portfolio/models.py
+++ portfolio/models.py
@@ -27,7 +27,6 @@ class Product(models.Model):
     size = models.CharField(max_length=200, null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     description = models.CharField(max_length=200, null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
 
