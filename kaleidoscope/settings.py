3b28da64d5e496577aaee2485d69d446a57dab17
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-18 14:51:25 +0000

    Stripe main functionallity completed

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -201,3 +201,8 @@ else:
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
