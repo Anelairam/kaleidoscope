e1e7e009264989aae402df83fa45249bb23d1c57
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-22 10:12:10 +0000

    Calendar view

--- cal/admin.py
+++ cal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'description',
+        'starting_time',
+        'ending_time',
+    )
+
+
+
+admin.site.register(Event, EventAdmin)
--- cal/urls.py
+++ cal/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.events_calendar, name='events_calendar'),
+    path(r'^calendar/$', views.CalendarView.as_view(), name='events_calendar'), # here
 ]
