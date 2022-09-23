d5452023dd97ff1495de6c7bd88e2c2f716dc4ad
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-23 08:47:46 +0000

    Commit before calendarium

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'day',
+        'starting_at',
+        'ending_at',
+        'description',
+    )
+
+
+admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=25)
+    day = models.DateField()
+    starting_at = models.TimeField()
+    ending_at = models.TimeField()
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
