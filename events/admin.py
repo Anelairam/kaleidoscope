362f1c9d4c80b0e68d2e577aa8cde20a9929fc6b
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-23 16:43:41 +0000

    Add difficulty to models

--- events/admin.py
+++ events/admin.py
@@ -1,15 +1,24 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Difficulty
 
 # Register your models here.
+
+class DifficultyAdmin(admin.ModelAdmin):
+    list_display = (
+        'difficulty',
+    )
+
+
 class EventAdmin(admin.ModelAdmin):
     list_display = (
         'title',
         'day',
+        'difficulty',
         'starting_at',
         'ending_at',
         'description',
     )
 
 
+admin.site.register(Difficulty, DifficultyAdmin)
 admin.site.register(Event, EventAdmin)
--- events/models.py
+++ events/models.py
@@ -1,10 +1,23 @@
+from portfolio.models import Type
 from django.db import models
-import calendar
+
 
 # Create your models here.
+class Difficulty(models.Model):
+    class Meta:
+        verbose_name_plural = 'Difficulty'
+
+    difficulty = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.difficulty
+
+
 class Event(models.Model):
     title = models.CharField(max_length=25)
     day = models.DateField()
+    type = models.ForeignKey(Type, null=True, on_delete=models.SET_NULL)
+    difficulty = models.ForeignKey('Difficulty', null=True, on_delete=models.SET_NULL)
     starting_at = models.TimeField()
     ending_at = models.TimeField()
     description = models.CharField(max_length=500)
@@ -13,36 +26,3 @@ class Event(models.Model):
         return self.title
 
 
-class Calendar(calendar.HTMLCalendar):
-    def __init__(self, year=None, month=None):
-        self.year = year
-        self.month = month
-        super(Calendar, self).__init__()
-        
-    
-    def formatday(self, day, events):
-        daily_events = events.filter(starting_at__day=day)
-        d = ''
-        for event in daily_events:
-            d += f'<li> {event.title} </li>'
-
-        if day != 0:
-            return f"<td><span class='day'>{day}</span><ul> {d} </ul></td>"
-        return '<td></td>'
-
-    def formatweek(self, theweek, events):
-        w = ''
-        for d, weekday in theweek:
-            w += self.formatday(d, events)
-        return f'<tr> {w} </tr>'
-
-    def formatmonth(self, withyear=True):
-        events = Event.objects.filter(starting_at__year=self.year, starting_at__month=self.month)
-
-        cd = f'<table class="cal">\n'
-        cd += f'{self.formatmonthname(self.year, self.month, withyear=withyear)}/n'
-        cd += f'{self.formatweekheader()}\n'
-        for week in self.monthdays2calendar(self.year, self.month):
-            cd += f'{self.formatweek(w, events)}\n'
-        return cd
-        
