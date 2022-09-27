4de5a4d0a7c129eb86f495f5261c40ce2cdcfc26
Marialena Livathinopoulou <m.livathinopoulou@hotmail.com>
2022-09-27 08:43:02 +0000

    Set Debug to False

--- kaleidoscope/settings.py
+++ kaleidoscope/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = ['https://8000-anelairam-kaleidoscope-i6yrovy3vqg.ws-eu67.gitpod.io']
 
